409c68f9aa452d99c21b504f23d4456175ff7eab
ddh963963 <ddh963963@naver.com>
2022-10-26 01:48:47 +0900

    initial project

---
+++ src/main/java/kr/flab/tradingmarket/TradingMarketApplication.java
@@ -0,0 +1,13 @@
+package kr.flab.tradingmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/flab/tradingmarket/TradingMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.flab.tradingmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

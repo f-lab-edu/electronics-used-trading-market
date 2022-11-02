5f3f9dab09fa38f0600da013e9f95e7c346667aa
ddh963963 <ddh963963@naver.com>
2022-11-03 02:55:21 +0900

    Feat : [#6] @ControllerAdvice를 이용한 Exception 통합 처리 로직 구현
    중복아이디 - UserIdDuplicateException 추가
    Validation - MethodArgumentNotValidException 추가

---
+++ src/main/java/kr/flab/tradingmarket/common/advice/ExceptionAdvices.java
@@ -0,0 +1,47 @@
+package kr.flab.tradingmarket.common.advice;
+
+import kr.flab.tradingmarket.common.code.ResponseMessage;
+import kr.flab.tradingmarket.domain.user.exception.UserIdDuplicateException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static kr.flab.tradingmarket.common.code.ResponseMessage.Status.FAIL;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+/**
+ * packageName :  kr.flab.tradingmarket.common.advice
+ * fileName : ExceptionAdvices
+ * author :  ddh96
+ * date : 2022-11-02
+ * description : Exception 통합 처리 Advice
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2022-11-02                ddh96             최초 생성
+ */
+@ControllerAdvice
+@Slf4j
+public class ExceptionAdvices {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ResponseMessage> validation(MethodArgumentNotValidException ex) {
+        log.info("validation ex : {}", ex.getFieldError());
+        return ResponseEntity.status(BAD_REQUEST).body(
+                new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
+                        .message("유효성 검증 실패")
+                        .validation(ex.getBindingResult().getFieldErrors())
+                        .build());
+    }
+
+    @ExceptionHandler(UserIdDuplicateException.class)
+    protected ResponseEntity<ResponseMessage> validation(UserIdDuplicateException ex) {
+        log.info("userIdDuplicate ex : ", ex);
+        return ResponseEntity.status(BAD_REQUEST).body(
+                new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
+                        .message("아이디가 중복됩니다.")
+                        .build());
+    }
+}

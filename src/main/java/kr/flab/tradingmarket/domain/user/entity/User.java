171b34f9f5c7a240eff51f9d9fd2920249e7653e
ddh963963 <ddh963963@naver.com>
2022-11-03 00:39:18 +0900

    Feat : [#3] User, UserProfileImage 엔티티 생성

---
+++ src/main/java/kr/flab/tradingmarket/domain/user/entity/User.java
@@ -0,0 +1,63 @@
+package kr.flab.tradingmarket.domain.user.entity;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * packageName :  kr.flab.tradingmarket.domain.user.entity
+ * fileName : User
+ * author :  ddh96
+ * date : 2022-11-02
+ * description : User 엔티티
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2022-11-02                ddh96             최초 생성
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+public class User {
+
+    private Long userNo;
+
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}",
+            message = "아이디는 영문과 숫자 조합으로 8 ~ 20자리까지 가능합니다.")
+    @NotBlank
+    private String userId;
+
+    @Length(max = 19)
+    @NotBlank
+    private String userName;
+    @NotBlank
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}",
+            message = "비밀번호는 영문과 숫자 조합으로 8 ~ 16자리까지 가능합니다.")
+    private String userPassword;
+
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{11,11}",
+            message = "전화번호는 숫자 11자리로 입력해주세요")
+    private String userPhone;
+
+    @NotNull
+    private LocalDate userBirth;
+
+
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+    private UserProfileImage userProfileImage;
+
+    public void setEncryptionPassword(String encryptionPassword) {
+        this.userPassword = encryptionPassword;
+    }
+
+}

d411ba6a8d57b900c346591a63ab58a51ec0162f
ddh96 <ddh963963@naver.com>
2022-11-14 15:43:01 +0900

    Feat : [#4] login 관련 ExceptionHandler 추가 및 컨트롤러 추가

--- src/main/java/kr/flab/tradingmarket/common/advice/ExceptionAdvices.java
+++ src/main/java/kr/flab/tradingmarket/common/advice/ExceptionAdvices.java
@@ -1,7 +1,9 @@
 package kr.flab.tradingmarket.common.advice;
 
 import kr.flab.tradingmarket.common.code.ResponseMessage;
+import kr.flab.tradingmarket.domain.user.exception.PasswordNotMatchException;
 import kr.flab.tradingmarket.domain.user.exception.UserIdDuplicateException;
+import kr.flab.tradingmarket.domain.user.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +14,7 @@ import java.time.format.DateTimeParseException;
 
 import static kr.flab.tradingmarket.common.code.ResponseMessage.Status.FAIL;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @ControllerAdvice
 @Slf4j
@@ -42,6 +45,24 @@ public class ExceptionAdvices {
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
                         .message("유효성 검증 실패 : 날짜 필드 오류")
+                        .build());
+    }
+
+    @ExceptionHandler(PasswordNotMatchException.class)
+    protected ResponseEntity<ResponseMessage> validation(PasswordNotMatchException ex) {
+        log.info("userIdDuplicate ex : ", ex);
+        return ResponseEntity.status(BAD_REQUEST).body(
+                new ResponseMessage.Builder(FAIL, UNAUTHORIZED.value())
+                        .message("비밀번호가 일치하지 않습니다.")
+                        .build());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    protected ResponseEntity<ResponseMessage> validation(UserNotFoundException ex) {
+        log.info("userIdDuplicate ex : ", ex);
+        return ResponseEntity.status(BAD_REQUEST).body(
+                new ResponseMessage.Builder(FAIL, UNAUTHORIZED.value())
+                        .message("해당 아이디가 존재하지 않습니다.")
                         .build());
     }
 }
--- src/main/java/kr/flab/tradingmarket/domain/user/controller/UserController.java
+++ src/main/java/kr/flab/tradingmarket/domain/user/controller/UserController.java
@@ -2,6 +2,8 @@ package kr.flab.tradingmarket.domain.user.controller;
 
 import kr.flab.tradingmarket.common.code.ResponseMessage;
 import kr.flab.tradingmarket.domain.user.dto.request.JoinUserDto;
+import kr.flab.tradingmarket.domain.user.dto.request.UserAuthDto;
+import kr.flab.tradingmarket.domain.user.service.LoginService;
 import kr.flab.tradingmarket.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import static org.springframework.http.HttpStatus.OK;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final LoginService loginService;
 
     @PostMapping("/join")
     public ResponseEntity<ResponseMessage> joinUser(@RequestBody @Valid JoinUserDto joinDto) {
@@ -28,5 +31,24 @@ public class UserController {
                         .Result(null)
                         .build());
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ResponseMessage> logout() {
+        loginService.logout();
+        return ResponseEntity.status(OK)
+                .body(new ResponseMessage.Builder(SUCCESS, OK.value())
+                        .Result(null)
+                        .build());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseMessage> login(@RequestBody @Valid UserAuthDto loginDto) {
+        loginService.login(loginDto);
+        return ResponseEntity.status(OK)
+                .body(new ResponseMessage.Builder(SUCCESS, OK.value())
+                        .Result(null)
+                        .build());
+    }
+
 
 }

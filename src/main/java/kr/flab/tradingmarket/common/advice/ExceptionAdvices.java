06103b890aa87c2cd7c84717fd1235fe49c46d27
ddh96 <ddh963963@naver.com>
2022-11-30 14:23:13 +0900

    Feat : [#10] ExceptionAdvices 전체 5xx Exception 추가 및 ImageUploadException,ExtensionNotSupportedException Hanlder 추가

--- src/main/java/kr/flab/tradingmarket/common/advice/ExceptionAdvices.java
+++ src/main/java/kr/flab/tradingmarket/common/advice/ExceptionAdvices.java
@@ -1,6 +1,8 @@
 package kr.flab.tradingmarket.common.advice;
 
 import kr.flab.tradingmarket.common.code.ResponseMessage;
+import kr.flab.tradingmarket.domain.image.exception.ExtensionNotSupportedException;
+import kr.flab.tradingmarket.domain.image.exception.ImageUploadException;
 import kr.flab.tradingmarket.domain.user.exception.PasswordNotMatchException;
 import kr.flab.tradingmarket.domain.user.exception.UserAccessDeniedException;
 import kr.flab.tradingmarket.domain.user.exception.UserIdDuplicateException;
@@ -14,16 +16,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.format.DateTimeParseException;
 
 import static kr.flab.tradingmarket.common.code.ResponseMessage.Status.FAIL;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 @Slf4j
 public class ExceptionAdvices {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ResponseMessage> validation(MethodArgumentNotValidException ex) {
-        log.info("validation ex : {}", ex.getFieldError());
+    protected ResponseEntity<ResponseMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.info("methodArgumentNotValidException ex : {}", ex.getFieldError());
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
                         .message("유효성 검증 실패")
@@ -32,8 +33,8 @@ public class ExceptionAdvices {
     }
 
     @ExceptionHandler(UserIdDuplicateException.class)
-    protected ResponseEntity<ResponseMessage> validation(UserIdDuplicateException ex) {
-        log.info("userIdDuplicate ex : ", ex);
+    protected ResponseEntity<ResponseMessage> userIdDuplicateException(UserIdDuplicateException ex) {
+        log.info("UserIdDuplicateException ex : ", ex);
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
                         .message("아이디가 중복됩니다.")
@@ -41,8 +42,8 @@ public class ExceptionAdvices {
     }
 
     @ExceptionHandler(DateTimeParseException.class)
-    protected ResponseEntity<ResponseMessage> validation(DateTimeParseException ex) {
-        log.info("userIdDuplicate ex : ", ex);
+    protected ResponseEntity<ResponseMessage> dateTimeParseException(DateTimeParseException ex) {
+        log.info("DateTimeParseException ex : ", ex);
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
                         .message("유효성 검증 실패 : 날짜 필드 오류")
@@ -50,8 +51,8 @@ public class ExceptionAdvices {
     }
 
     @ExceptionHandler(PasswordNotMatchException.class)
-    protected ResponseEntity<ResponseMessage> validation(PasswordNotMatchException ex) {
-        log.info("userIdDuplicate ex : ", ex);
+    protected ResponseEntity<ResponseMessage> passwordNotMatchException(PasswordNotMatchException ex) {
+        log.info("PasswordNotMatchException ex : ", ex);
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ResponseMessage.Builder(FAIL, UNAUTHORIZED.value())
                         .message("비밀번호가 일치하지 않습니다.")
@@ -59,8 +60,8 @@ public class ExceptionAdvices {
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    protected ResponseEntity<ResponseMessage> validation(UserNotFoundException ex) {
-        log.info("userIdDuplicate ex : ", ex);
+    protected ResponseEntity<ResponseMessage> userNotFoundException(UserNotFoundException ex) {
+        log.info("UserNotFoundException ex : ", ex);
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ResponseMessage.Builder(FAIL, UNAUTHORIZED.value())
                         .message("해당 아이디가 존재하지 않습니다.")
@@ -68,12 +69,44 @@ public class ExceptionAdvices {
     }
 
     @ExceptionHandler(UserAccessDeniedException.class)
-    protected ResponseEntity<ResponseMessage> validation(UserAccessDeniedException ex) {
-        log.info("userIdDuplicate ex : ", ex);
+    protected ResponseEntity<ResponseMessage> userAccessDeniedException(UserAccessDeniedException ex) {
+        log.info("UserAccessDeniedException ex : ", ex);
         return ResponseEntity.status(UNAUTHORIZED).body(
                 new ResponseMessage.Builder(FAIL, UNAUTHORIZED.value())
                         .message("접근 권한이 없습니다. 로그인해주세요")
                         .Result("/login")
                         .build());
     }
+
+    @ExceptionHandler(ExtensionNotSupportedException.class)
+    protected ResponseEntity<ResponseMessage> extensionNotSupportedException(ExtensionNotSupportedException ex) {
+        log.info("ExtensionNotSupportedException ex : ", ex);
+        return ResponseEntity.status(BAD_REQUEST).body(
+                new ResponseMessage.Builder(FAIL, BAD_REQUEST.value())
+                        .message(ex.getExtension() + "는 지원하지 않는 이미지 확장자입니다.")
+                        .Result("")
+                        .build());
+    }
+
+    @ExceptionHandler(ImageUploadException.class)
+    protected ResponseEntity<ResponseMessage> imageUploadException(ImageUploadException ex) {
+        log.info("ExtensionNotSupportedException ex : ", ex);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(
+                new ResponseMessage.Builder(FAIL, INTERNAL_SERVER_ERROR.value())
+                        .message("이미지 업로드에 실패했습니다. 재시도 해주세요.")
+                        .Result("")
+                        .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ResponseMessage> exception(Exception ex) {
+        log.error("Exception ex : ", ex);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(
+                new ResponseMessage.Builder(FAIL, INTERNAL_SERVER_ERROR.value())
+                        .message("서버 오류입니다. 복구될때까지 기다려주세요")
+                        .Result("")
+                        .build());
+    }
+
+
 }

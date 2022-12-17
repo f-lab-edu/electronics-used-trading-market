a92277a7ef0edf6cd4894a0a318e2a60638d3fe9
ddh96 <ddh963963@naver.com>
2022-12-17 23:04:14 +0900

    Feat : [#23] 카테고리 조회용 Controller / Service / Mapper 추가

---
+++ src/main/java/kr/flab/tradingmarket/domain/category/service/CategoryService.java
@@ -0,0 +1,13 @@
+package kr.flab.tradingmarket.domain.category.service;
+
+import java.util.List;
+
+import kr.flab.tradingmarket.domain.category.dto.response.SimpleCategoryDto;
+
+public interface CategoryService {
+    List<SimpleCategoryDto> findAllFirstCategories();
+
+    List<SimpleCategoryDto> findAllSecondCategories();
+
+    List<SimpleCategoryDto> findAllThirdCategories();
+}

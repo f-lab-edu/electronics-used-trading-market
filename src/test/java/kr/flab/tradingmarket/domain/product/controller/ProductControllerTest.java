1949ff8ae81101d8c0732e85679fb4f6364f33e7
ddh96 <ddh963963@naver.com>
2023-01-15 15:58:02 +0900

    Test : [#14] Modify Controller 테스트 추가

--- src/test/java/kr/flab/tradingmarket/domain/product/controller/ProductControllerTest.java
+++ src/test/java/kr/flab/tradingmarket/domain/product/controller/ProductControllerTest.java
@@ -32,14 +32,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.flab.tradingmarket.common.aop.LoginCheckAop;
+import kr.flab.tradingmarket.common.exception.DtoValidationException;
+import kr.flab.tradingmarket.common.validator.CategoryValidator;
+import kr.flab.tradingmarket.domain.category.service.CategoryService;
 import kr.flab.tradingmarket.domain.image.exception.ImageUploadException;
+import kr.flab.tradingmarket.domain.product.exception.ProductModifyException;
 import kr.flab.tradingmarket.domain.product.exception.ProductRegisterException;
 import kr.flab.tradingmarket.domain.product.service.ProductCommandService;
+import kr.flab.tradingmarket.domain.product.service.ProductService;
 import kr.flab.tradingmarket.domain.user.service.LoginService;
 
 @WebMvcTest(ProductController.class)
 @ExtendWith(MockitoExtension.class)
-@Import({AopAutoConfiguration.class, LoginCheckAop.class})
+@Import({AopAutoConfiguration.class, LoginCheckAop.class, CategoryValidator.class})
 @ActiveProfiles("test")
 class ProductControllerTest {
 
@@ -51,12 +56,22 @@ class ProductControllerTest {
     ObjectMapper objectMapper;
     @MockBean
     LoginService loginService;
-    MockMultipartFile productImageMultiFile;
+
+    @MockBean
+    ProductService productService;
+    @MockBean
+    CategoryService categoryService;
 
     @BeforeEach
     public void initMock() throws JsonProcessingException {
-        productImageMultiFile = new MockMultipartFile("data", "data",
-            "application/json", toJson(DEFAULT_REGISTER_PRODUCT_DTO).getBytes(StandardCharsets.UTF_8));
+        ;
+        given(categoryService.existCategory(any()))
+            .willReturn(true);
+    }
+
+    private MockMultipartFile makeRequestPartFixture(Object object) throws JsonProcessingException {
+        return new MockMultipartFile("data", "data",
+            "application/json", toJson(object).getBytes(StandardCharsets.UTF_8));
     }
 
     private String toJson(Object object) throws JsonProcessingException {
@@ -71,7 +86,7 @@ class ProductControllerTest {
         mockMvc.perform(multipart(HttpMethod.POST, DEFAULT_PRODUCT_URL).file(REGISTER_PRODUCT_MULTIPART_1ST)
                 .file(REGISTER_PRODUCT_MULTIPART_2ND)
                 .file(REGISTER_PRODUCT_MULTIPART_3RD)
-                .file(productImageMultiFile)
+                .file(makeRequestPartFixture(DEFAULT_REGISTER_PRODUCT_DTO))
                 .contentType("multipart/mixed")
                 .accept(MediaType.APPLICATION_JSON))
             .andDo(print())
@@ -101,7 +116,7 @@ class ProductControllerTest {
                 .file(REGISTER_PRODUCT_MULTIPART_3RD)
                 .file(REGISTER_PRODUCT_MULTIPART_1ST)
                 .file(REGISTER_PRODUCT_MULTIPART_2ND)
-                .file(productImageMultiFile)
+                .file(makeRequestPartFixture(DEFAULT_REGISTER_PRODUCT_DTO))
                 .contentType("multipart/mixed")
                 .accept(MediaType.APPLICATION_JSON))
             .andDo(print())
@@ -122,7 +137,7 @@ class ProductControllerTest {
         //when
         mockMvc.perform(multipart(HttpMethod.POST, DEFAULT_PRODUCT_URL)
                 .file(NO_IMAGE)
-                .file(productImageMultiFile)
+                .file(makeRequestPartFixture(DEFAULT_REGISTER_PRODUCT_DTO))
                 .contentType("multipart/mixed")
                 .accept(MediaType.APPLICATION_JSON))
             .andDo(print())
@@ -148,7 +163,7 @@ class ProductControllerTest {
                 .file(REGISTER_PRODUCT_MULTIPART_1ST)
                 .file(REGISTER_PRODUCT_MULTIPART_2ND)
                 .file(REGISTER_PRODUCT_MULTIPART_3RD)
-                .file(productImageMultiFile)
+                .file(makeRequestPartFixture(DEFAULT_REGISTER_PRODUCT_DTO))
                 .contentType("multipart/mixed")
                 .accept(MediaType.APPLICATION_JSON))
             .andDo(print())
@@ -173,7 +188,7 @@ class ProductControllerTest {
                 .file(REGISTER_PRODUCT_MULTIPART_1ST)
                 .file(REGISTER_PRODUCT_MULTIPART_2ND)
                 .file(REGISTER_PRODUCT_MULTIPART_3RD)
-                .file(productImageMultiFile)
+                .file(makeRequestPartFixture(DEFAULT_REGISTER_PRODUCT_DTO))
                 .contentType("multipart/mixed")
                 .accept(MediaType.APPLICATION_JSON))
             .andDo(print())
@@ -183,6 +198,172 @@ class ProductControllerTest {
         then(productCommandService)
             .should()
             .registerProduct(any(), any(), any());
+    }
+
+    @Test
+    @DisplayName("controller : 수정 상품 조회 테스트 : 성공")
+    void successfulGetModifyProduct() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(true);
+
+        //when
+        mockMvc.perform(get(productEditUrl(1L)))
+            .andDo(print())
+            .andExpect(status().isOk());
+
+        //then
+        then(productCommandService)
+            .should()
+            .findByModifyProduct(any());
+    }
+
+    @Test
+    @DisplayName("controller : 수정 상품 조회 테스트 : 권한 없음 실패")
+    void failGetModifyProductByNoPermission() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(false);
+
+        //when
+        mockMvc.perform(get(productEditUrl(1L)))
+            .andDo(print())
+            .andExpect(status().isUnauthorized());
+
+        //then
+        then(productCommandService)
+            .should(never())
+            .findByModifyProduct(any());
+    }
+
+    @Test
+    @DisplayName("controller : 상품 수정 테스트 : 성공")
+    void successfulModifyProduct() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(true);
+
+        //when
+        mockMvc.perform(multipart(HttpMethod.PUT, productEditUrl(1L))
+                .file(REGISTER_PRODUCT_MULTIPART_1ST)
+                .file(REGISTER_PRODUCT_MULTIPART_2ND)
+                .file(REGISTER_PRODUCT_MULTIPART_3RD)
+                .file(makeRequestPartFixture(DEFAULT_REQUEST_MODIFY_PRODUCT_DTO))
+                .contentType("multipart/mixed")
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk());
+
+        //then
+        then(productCommandService)
+            .should()
+            .modifyProduct(any(), any(), any());
+
+    }
+
+    @Test
+    @DisplayName("controller : 상품 수정 테스트 : 권한 없음 실패")
+    void failModifyProductByNoPermission() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(false);
+
+        //when
+        mockMvc.perform(multipart(HttpMethod.PUT, productEditUrl(1L))
+                .file(REGISTER_PRODUCT_MULTIPART_1ST)
+                .file(REGISTER_PRODUCT_MULTIPART_2ND)
+                .file(REGISTER_PRODUCT_MULTIPART_3RD)
+                .file(makeRequestPartFixture(DEFAULT_REQUEST_MODIFY_PRODUCT_DTO))
+                .contentType("multipart/mixed")
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isUnauthorized());
+
+        //then
+        then(productCommandService)
+            .should(never())
+            .modifyProduct(any(), any(), any());
+    }
+
+    @Test
+    @DisplayName("controller : 상품 수정 테스트 : 상품 수정 실패")
+    void failModifyProductByProductModifyException() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(true);
+        willThrow(ProductModifyException.class)
+            .given(productCommandService)
+            .modifyProduct(any(), any(), any());
+
+        //when
+        mockMvc.perform(multipart(HttpMethod.PUT, productEditUrl(1L))
+                .file(REGISTER_PRODUCT_MULTIPART_1ST)
+                .file(REGISTER_PRODUCT_MULTIPART_2ND)
+                .file(REGISTER_PRODUCT_MULTIPART_3RD)
+                .file(makeRequestPartFixture(DEFAULT_REQUEST_MODIFY_PRODUCT_DTO))
+                .contentType("multipart/mixed")
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isInternalServerError());
+
+        //then
+        then(productCommandService)
+            .should()
+            .modifyProduct(any(), any(), any());
+    }
+
+    @Test
+    @DisplayName("controller : 상품 수정 테스트 : validation 상품 수정 실패")
+    void failModifyProductByDtoValidationException() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(true);
+        willThrow(new DtoValidationException("testFiled", "testMessage"))
+            .given(productCommandService)
+            .modifyProduct(any(), any(), any());
+
+        //when
+        mockMvc.perform(multipart(HttpMethod.PUT, productEditUrl(1L))
+                .file(REGISTER_PRODUCT_MULTIPART_1ST)
+                .file(REGISTER_PRODUCT_MULTIPART_2ND)
+                .file(REGISTER_PRODUCT_MULTIPART_3RD)
+                .file(makeRequestPartFixture(DEFAULT_REQUEST_MODIFY_PRODUCT_DTO))
+                .contentType("multipart/mixed")
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+
+        //then
+        then(productCommandService)
+            .should()
+            .modifyProduct(any(), any(), any());
+    }
+
+    @Test
+    @DisplayName("controller : 상품 수정 테스트 : 이미지 업로드 실패")
+    void failModifyProductByImageUploadException() throws Exception {
+        //given
+        given(productService.isProductAuthorized(any(), any()))
+            .willReturn(true);
+        willThrow(ImageUploadException.class)
+            .given(productCommandService)
+            .modifyProduct(any(), any(), any());
+
+        //when
+        mockMvc.perform(multipart(HttpMethod.PUT, productEditUrl(1L))
+                .file(REGISTER_PRODUCT_MULTIPART_1ST)
+                .file(REGISTER_PRODUCT_MULTIPART_2ND)
+                .file(REGISTER_PRODUCT_MULTIPART_3RD)
+                .file(makeRequestPartFixture(DEFAULT_REQUEST_MODIFY_PRODUCT_DTO))
+                .contentType("multipart/mixed")
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isInternalServerError());
+
+        //then
+        then(productCommandService)
+            .should()
+            .modifyProduct(any(), any(), any());
     }
 
 }

dcaa1eae523f42f9688f0c1bbcecdffe91ee9d5d
HYKIM <ddh963963@naver.com>
2023-03-10 02:54:02 +0900

    Refactor : [#34] Interface 캡슐화

--- src/main/java/kr/flab/tradingmarket/domain/room/repository/RoomRepository.java
+++ src/main/java/kr/flab/tradingmarket/domain/room/repository/RoomRepository.java
@@ -1,37 +1,15 @@
 package kr.flab.tradingmarket.domain.room.repository;
 
-import static kr.flab.tradingmarket.domain.chat.util.RedisKeyUtils.*;
-
 import java.util.Set;
 
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Repository;
-
 import kr.flab.tradingmarket.domain.room.entity.RoomKey;
-import lombok.RequiredArgsConstructor;
 
-@Repository
-@RequiredArgsConstructor
-public class RoomRepository {
+public interface RoomRepository {
+    Set<String> findByUserNo(Long userNo);
 
-    private final RedisTemplate<String, String> redisChatTemplate;
+    boolean isRoomExists(String roomId);
 
-    public Set<String> findByUserNo(Long userNo) {
-        return redisChatTemplate.opsForSet().members(createUserRoomKey(userNo));
-    }
+    boolean isRoomExistsByUserNoAndUserNo(String roomId, Long userNo);
 
-    public boolean isRoomExists(String roomId) {
-        return Boolean.TRUE.equals(redisChatTemplate.hasKey(createRoomKey(roomId)));
-    }
-
-    public boolean isRoomExistsByUserNoAndUserNo(String roomId, Long userNo) {
-        return Boolean.TRUE.equals(redisChatTemplate.opsForSet().isMember(createUserRoomKey(userNo), roomId));
-    }
-
-    public String createRooms(RoomKey roomKey) {
-        redisChatTemplate.opsForSet().add(createUserRoomKey(roomKey.getBuyerNo()), roomKey.getRoomId());
-        redisChatTemplate.opsForSet().add(createUserRoomKey(roomKey.getSellerNo()), roomKey.getRoomId());
-        return roomKey.getRoomId();
-    }
-
+    String createRooms(RoomKey roomKey);
 }

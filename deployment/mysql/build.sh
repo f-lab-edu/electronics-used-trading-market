962c7596fc4c79b0d4c710f38aa504b75aed67d8
HYKIM <ddh963963@naver.com>
2023-04-11 01:35:40 +0900

    Build : docker 기반 mysql replication 설정 파일 추가

---
+++ deployment/mysql/build.sh
@@ -0,0 +1,37 @@
+docker-compose down -v
+rm -rf ./master/data/*
+rm -rf ./slave/data/*
+docker-compose build
+docker-compose up -d
+
+until docker exec mysql_master sh -c 'export MYSQL_PWD=#password; mysql -u root -e ";"'
+do
+    echo "Master DB 접속중..."
+    sleep 10
+done
+
+priv_stmt='CREATE USER "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
+
+docker exec mysql_master sh -c "export MYSQL_PWD=#password; mysql -u root -e '$priv_stmt'"
+
+until docker-compose exec mysql_slave sh -c 'export MYSQL_PWD=#password; mysql -u root -e ";"'
+do
+    echo "Slave DB 접속중..."
+    sleep 10
+done
+
+MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=#password; mysql -u root -e "SHOW MASTER STATUS"'`
+CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+
+# ip 계정 정보들 입력
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='root',MASTER_PASSWORD='#password',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS,GET_MASTER_PUBLIC_KEY=1; START SLAVE;"
+start_slave_cmd='export MYSQL_PWD=#password; mysql -u root -e "'
+start_slave_cmd+="$start_slave_stmt"
+start_slave_cmd+='"'
+
+docker exec mysql_slave sh -c "$start_slave_cmd"
+
+docker exec mysql_slave sh -c "export MYSQL_PWD=#password; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+
+
